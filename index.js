959c61849324aadf4decd00571f3a487e4d724e3
manish kumar ravi <manishkumarravi1016@gmail.com>
2024-11-04 14:00:09 +0530

    create server

---
+++ index.js
@@ -0,0 +1,15 @@
+const exp = require('constants');
+const express = require('express');
+const port = 8000;
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send("cool it is running")
+})
+
+app.listen(port, function (err) {
+    if (err) {
+        console.log("Error in running the server", err)
+    }
+    console.log(`Server is running on port: ${port}`);
+})
