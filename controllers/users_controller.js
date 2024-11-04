644cb320e0a8a3a4fd3474c1362e4a2045115ad2
manish kumar ravi <manishkumarravi1016@gmail.com>
2024-11-04 15:45:25 +0530

    add footer and sign up page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,6 +1,15 @@
+const { title } = require("process");
+
 module.exports.signIn = (req, res) => {
 
     return res.render('user_sign_in', {
         title: 'Review system | Sign In'
     });
+}
+
+module.exports.signUp = (req, res) => {
+
+    return res.render('user_sign_up', {
+        title: 'Review system | Sign Up'
+    })
 }
--- routes/users.js
+++ routes/users.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const userController = require('../controllers/users_controller')
 
 router.get('/', userController.signIn);
+router.get('/sign-up', userController.signUp);
 module.exports = router;
