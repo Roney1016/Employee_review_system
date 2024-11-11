3a72b869bfb9e86569472c9b7a56cea3c68d250c
manish kumar ravi <manishkumarravi1016@gmail.com>
2024-11-11 21:48:52 +0530

    finish

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-require('dotenv').config();
+
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const cookieParser = require('cookie-parser'); // parse cookie header and populate req.cookies
